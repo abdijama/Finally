b85dab66b15e82781e7b3ff03439ddd3bf301f2b
abdijama <abdi.jama@techhigh.us>
2015-09-04 08:08:09 -0400

    Do While Loops

---
+++ Do While Loops/apples.java
@@ -0,0 +1,12 @@
+public class apples {
+	public static void main (String[] args) {
+		int counter = 15;
+		
+		do {
+			System.out.println(counter);
+			counter++;
+			
+		}
+		while (counter <=10);
+	}
+}
