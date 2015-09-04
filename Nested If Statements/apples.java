17160caa38f1cc7e25e30ce24c606f11d9bd0de1
abdijama <abdi.jama@techhigh.us>
2015-09-04 08:08:41 -0400

    Nested If Statements

---
+++ Nested If Statements/apples.java
@@ -0,0 +1,17 @@
+public class apples {
+	public static void main(String[] args) {
+		int age = 60;
+		if (age < 50) {
+			System.out.println("You are young");
+		}
+		else {
+			System.out.println("You are old");
+			if (age > 75) {
+				System.out.println("You are really old!");
+			}
+			else {
+				System.out.println("Dont worry you arent really that old");
+			}
+		}
+	}
+}
